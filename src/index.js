d350e4372c2b0fd4e60c7b65e8ff43279a87aaa1
Ilya Starickov <ilyastarickov1996@gmail.com>
2019-03-20 23:40:59 +0400

    completed step3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-	const askUserName = readlineSync.question("May I have your name?");
-	console.log('Hi, ' + askUserName +'!');
+const askName = () => {
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hi, ${userName}!`);
 };
+export default askName;
